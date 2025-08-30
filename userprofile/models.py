a404efc91b6bc083204fa6ad22b429c85075513b
Anas <86509653+AnasDharar@users.noreply.github.com>
2025-08-30 14:06:58 +0530

    database

--- userprofile/models.py
+++ userprofile/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Student(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    test1 = models.FloatField(blank=True, null=True)
+    test2 = models.FloatField(blank=True, null=True)
+    test3 = models.FloatField(blank=True, null=True)
+    test4 = models.FloatField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s scores"
